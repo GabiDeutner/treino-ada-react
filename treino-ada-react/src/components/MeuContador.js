7f10a42bf5742c9e4e3cb2dff612fb268db90ad9
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-11-17 18:12:13 -0300

    Renderização condicional

--- treino-ada-react/src/components/MeuContador.js
+++ treino-ada-react/src/components/MeuContador.js
@@ -9,14 +9,38 @@ export default function MeuContador(){
         setContador(contador + 1)
     }
 
-    return (
+    function diminuir(){
+        setContador(contador - 1)
+    }
+
+    if(contador > 5){
+        return(
         <div>
-            <h1>Meu Contador</h1>
-            <h3>{contador}</h3>
+            <h1>Valor muito alto!</h1>
+            <button onClick={aumentar}>aumentar</button>
+            <button onClick={diminuir}>diminuir</button>
+        </div>
+        )
+    } 
+
+
+        return(
+        <div>
+            <h1>Meu Contador:</h1>
+            <button onClick={aumentar}>aumentar</button>
+            <button onClick={diminuir}>diminuir</button>
+        </div>
+        )
+    }
+
+   /* return (
+        <div>
+            <h1>Meu Contador: {contador}</h1>
+            { contador > 9 ? <h1>Valor muito grande</h1> : null}
             <button onClick={aumentar}>aumentar</button>
         </div>
-    )
-  }
+    )*/
+  
 
 
   // hooks - variáveis de estado (faz o componente ser renderizado para mudança de estado de uma const)
