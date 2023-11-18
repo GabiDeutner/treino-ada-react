b93b6ce9b8854565794e85db37d662c14aabc3be
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-11-17 21:29:13 -0300

    listas e chaves

--- treino-ada-react/src/App.js
+++ treino-ada-react/src/App.js
@@ -1,11 +1,13 @@
 import MeuComponente from './components/MeuComponente';
 import MeuContador from './components/MeuContador'; 
+import MinhaLista from './components/MinhaLista';
 
 function App() {
   return (
     <div>
     <h1>Hello World, React!</h1>
-    
+    <h1>Listas no React</h1>
+    <MinhaLista/>
     <MeuComponente />
     <MeuBotao conteudo='me clique' />
     <MeuBotao conteudo='depois aqui' />
