a99b726c4d1714420d43cc64059033fcf4cd1d0d
Gabriela Paulini Deutner <deutnerg@gmail.com>
2023-11-22 23:07:28 -0300

    ajustes

--- treino-ada-react/src/components/MinhaLista.js
+++ treino-ada-react/src/components/MinhaLista.js
@@ -1,17 +1,11 @@
-import { useState } from "react"
-
 const minhaLista = [
     {id: '1', value: 'Fruta'},
     {id: '2', value: 'Legume'},
     {id: '3', value: 'Carne'},
 ]
 
-const [produtos, setProdutos] = useState(minhaLista)
-
 export default function MinhaLista(){
-
-    return (
-    minhaLista.map( (item) => {
+    return minhaLista.map( (item) => {
         return(
             <div key={item.id}>
                 <h4>{item.value}</h4>
@@ -19,6 +13,14 @@ export default function MinhaLista(){
         )
     } )
 
-  
-    )
+    /*const minhaLista = [
+        <h4 key="1">Item 1</h4>,
+        <h4 key="2">Item 2</h4>,
+        <h4 key="3">Item 3</h4>,
+        <h4 key="4">Item 4</h4>,
+        <h4 key="5">Item 5</h4>
+
+    ]*/
+    return minhaLista
+
 }
